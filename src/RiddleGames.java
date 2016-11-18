c6aa05a8f0ef7518696e24e7e11cb6390f6f7edb
diah022 <lee.diah05@gmail.com>
2016-11-18 14:16:09 +0700

    12141363 fungsi input data

--- src/RiddleGames.java
+++ src/RiddleGames.java
@@ -3,5 +3,9 @@ public class RiddleGames {
     private String nama;
     private int tebakAngka;
     private byte level;
-        
+    
+   // fungsi input data
+    public void input(){
+       
+   } 
 }

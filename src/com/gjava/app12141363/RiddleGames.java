67ea0b5a171e34a06293d38e82ad4123a6829839
diah022 <lee.diah05@gmail.com>
2016-11-18 15:30:01 +0700

    12141363 random angka

---
+++ src/com/gjava/app12141363/RiddleGames.java
@@ -0,0 +1,44 @@
+package com.gjava.app12141363;
+
+
+import com.sun.xml.internal.fastinfoset.stax.events.Util;
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class RiddleGames {
+    private String nama;
+    private int tebakAngka;
+    private byte level; 
+    private char jawab;
+    public int randomisedNumber;
+    
+   // fungsi input data
+    public void input(){
+       // input nama pemain
+        Scanner name = new Scanner(System.in);
+        System.out.println("Masukkan Nama Anda untuk memulai atau tekan 'Y' untuk keluar. Kemudian tekan 'enter'");
+        nama = name.nextLine();
+        
+        // input angka tebakan
+        Scanner inputAngka = new Scanner(System.in);
+        System.out.println("Masukkan angka tebakan : ");
+        tebakAngka = inputAngka.nextInt();
+   } 
+    
+    // fungsi proses data
+    public void proses(){
+        Random randomGenerator = new Random();
+        this.randomisedNumber = randomGenerator.nextInt();
+        
+    }
+    
+    // fungsi untuk output data
+    public void output(){
+        // pilihan untuk keluar atau lanjut
+        System.out.println(nama);
+        if(!Util.isEmptyString(nama)){
+            
+        }
+    }
+}

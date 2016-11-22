3cba184ae258c6884c4af380cbb4d8a13353275b
diah022 <lee.diah05@gmail.com>
2016-11-22 14:36:54 +0700

    12141363 variabel

---
+++ src/com/gjava/app12141363/InputPemain.java
@@ -0,0 +1,16 @@
+
+package com.gjava.app12141363;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author D
+ */
+public class InputPemain {
+    private List<Pemain> pemain = new ArrayList<>();
+    private Pemain gamers;
+
+    
+}

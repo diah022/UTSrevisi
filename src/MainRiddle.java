03b88fbfabfa6be45b8dba6b6b3232fe1eb832b5
diah022 <lee.diah05@gmail.com>
2016-11-22 14:27:01 +0700

    12141363 variabel

--- src/MainRiddle.java
+++ src/MainRiddle.java
@@ -7,7 +7,15 @@ import java.util.Scanner;
 public class MainRiddle {
     public static void main(String[] args) {
 //        char jawab;
+        String nama;
+        int tebakAngka;
+        int level = 1; 
+        int randomisedNumber;
+        int jumlahTebakan;
+        int kesempatan;
+        int cekGameOver = 0;
         
+       
         
     }
 }

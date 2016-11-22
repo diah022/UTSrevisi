3377ceb46716e3d6f67dbed0c6574d93819620f7
diah022 <lee.diah05@gmail.com>
2016-11-22 14:40:43 +0700

    12141363 atribut detailscore

---
+++ src/com/gjava/app12141363/DetailScore.java
@@ -0,0 +1,14 @@
+
+package com.gjava.app12141363;
+
+/**
+ *
+ * @author D
+ */
+public class DetailScore {
+//    private AngkaTebakan angkaTebakan;
+    private int level;
+    private int jumlahTebakan;
+
+    
+}

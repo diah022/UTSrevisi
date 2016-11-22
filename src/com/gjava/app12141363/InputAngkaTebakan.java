a2451a4f7eaddc32e14366f0f04f9fa185bad897
diah022 <lee.diah05@gmail.com>
2016-11-22 14:42:45 +0700

    12141363 atribut

---
+++ src/com/gjava/app12141363/InputAngkaTebakan.java
@@ -0,0 +1,16 @@
+
+package com.gjava.app12141363;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author D
+ */
+public class InputAngkaTebakan {
+    private List<AngkaTebakan> angkaTebakan = new ArrayList<>();
+    private AngkaTebakan angg;
+
+    
+}

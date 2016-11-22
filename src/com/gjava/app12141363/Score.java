627b4ac3ffced956efa95373fcce8f93fe29763c
diah022 <lee.diah05@gmail.com>
2016-11-22 14:39:28 +0700

    12141363 atribut

---
+++ src/com/gjava/app12141363/Score.java
@@ -0,0 +1,18 @@
+
+package com.gjava.app12141363;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author D
+ */
+public class Score {
+    private Pemain pemain;
+    private List<DetailScore> detailScore = new ArrayList<>();
+
+    
+    
+    
+}
